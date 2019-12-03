930f20b4827826d0ff9f06806ae8db7d85e2ed8d
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-12-03 14:31:08 +0000

    Adding the form submit logic

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 let myLibrary = [];
 
-function Book(){
+class Book {
     constructor(title, author)
     {
         this.title = title; 
         this.author = author;
     }
 }
+
  
- const addButton = document.getElementById("addBook").addEventListener("")
- const form = document.getElementById("form")
-  form.addEventListener("click", myFunction())
-  function myFunction(){
-       
-        if (form.style.visibility === "hidden") {
-        form.style.visibility = "visible"
+ const addBook = document.getElementById("addBook");
+ const form = document.getElementById("form");
+ const showFormButton = document.getElementById("showForm")
+ addBook.addEventListener('click', addBookToLibrary());
+  showFormButton.addEventListener("click", function(){
+        if (form.style.display === "block") {
+        form.style.display = "none"
+        showFormButton.innerHTML = "Show Form"
+
         } 
         else {
-        form.style.visibility = "hidden"
+        form.style.display = "block"
+        showFormButton.innerHTML = "Hide Form"
         }
-   }
+   });
+
+
+
     function addBookToLibrary() {
-    let newBook = new Book('title', 'author')
-    myLibrary.push(newBook)
-}
+        const title = document.getElementById("title").value
+        const author = document.getElementById("author").value
+        console.log(title)
+            const newBook = new Book(title, author)
+            console.log(newBook)
+            myLibrary.push(newBook);
+            console.log(myLibrary)
+    
+
+    }
+
 
