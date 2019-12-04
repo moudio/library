292aff621e8c6e3bf78490fc3cd8d090f88545f7
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2019-12-04 10:03:33 +0000

    Adding the render function

--- script.js
+++ script.js
@@ -1,49 +1,58 @@
+// VARIABLES
+const addBook = document.getElementById("addBook");
+const form = document.getElementById("form");
+const author = document.getElementById("author");
+const title = document.getElementById("title")
+const showFormButton = document.getElementById("showForm");
+const showLibrary = document.getElementById("showLibrary");
+const libraryDiv = document.getElementById("library");
+const libraryBody = document.getElementById("library-table-body")
 let myLibrary = [];
 
-class Book {
-    constructor(title, author)
-    {
-        this.title = title; 
-        this.author = author;
+
+// Event Listeners 
+showLibrary.addEventListener("click", listOutBooks);
+addBook.addEventListener("click", addBookToLibrary);
+showFormButton.addEventListener("click", function() {
+    if (form.style.display === "block") {
+      form.style.display = "none";
+      showFormButton.innerHTML = "Show Form";
+    } else {
+      form.style.display = "block";
+      showFormButton.innerHTML = "Hide Form";
     }
+  });
+
+
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
- const showLibrary = document.getElementById("showLibrary")
-  showLibrary.addEventListener('click', listOutBooks)
- 
- const addBook = document.getElementById("addBook");
- const form = document.getElementById("form");
- const showFormButton = document.getElementById("showForm")
- addBook.addEventListener('click', addBookToLibrary);
-  showFormButton.addEventListener("click", function(){
-        if (form.style.display === "block") {
-        form.style.display = "none"
-        showFormButton.innerHTML = "Show Form"
-
-        } 
-        else {
-        form.style.display = "block"
-        showFormButton.innerHTML = "Hide Form"
-        }
-   });
 
 
 
-    function addBookToLibrary() {
-        const title = document.getElementById("title").value
-        const author = document.getElementById("author").value
-        console.log(title)
-            const newBook = new Book(title, author)
-            console.log(newBook)
-            myLibrary.push(newBook);
-            console.log(myLibrary)
+
+function addBookToLibrary() {
+  const titleValue = title.value;
+  const authorValue = author.value
+  const newBook = new Book(titleValue, authorValue);
+  myLibrary.push(newBook);
+  title.value = "";
+  author.value = "";
+}
+
+
+function listOutBooks() {
+  for (let book of myLibrary) {
+    
+    libraryBody.innerHTML += `<td>${book.title} </td>
+    <td> ${book.author} </td>
+    <td> <button class="remove-book'> Remove </button>`
+
     
 
-    }
-
-    function listOutBooks(){
-        // for ( let book of myLibrary ) {
-            console.log (myLibrary)
-        // }
-    }
-
-
+  }
+}
